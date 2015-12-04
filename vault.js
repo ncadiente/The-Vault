e5aff51cc4256dea3ef8f0ff7900100482917e14
Nick Cadiente <nickcadiente@Nicks-MacBook-Pro.local>
2015-12-03 18:26:34 -1000

    passes tests

--- vault.js
+++ vault.js
@@ -1,4 +1,15 @@
-'use strict';
 module.exports = function() {
-  
+'use strict';
+var myVault = {};
+  return {
+    setValue : function(key, value) {
+     myVault[key] = value;
+    },
+    getValue : function (key) {
+      if (myVault.hasOwnProperty(key)){
+        return myVault[key];
+      }
+      return null;
+    }
+  };
 };
